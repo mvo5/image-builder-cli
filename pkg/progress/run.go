3f6ccd4f469728be72fb66b5af45c52b51a8353f
Michael Vogt <michael.vogt@gmail.com>
2025-04-07 16:30:33 +0200

    progress: add `IMAGE_BUILDER_EXPERIMENTAL=supermin` rootless builds
    This adds support to build images without root by using `supermin`
    and a virtual machine. With `supermin` we can create a minimal
    VM with osbuild and all supported pacakge very quickly and then
    map /store and /output in.
    
    This works on a regular system with a uid != 0 and /dev/kvm access
    and inside a container that has access to /dev/kvm.
    
    There are a few wrinkles:
    - /dev/kvm must be available for the VM or things are really slow
    - /store cannot be mounted from the host and mounted directly because
      there will be selinux label/uid map issues between VM and host
    - progress reporting is currently not supported (but adding it
      should be conceptually not very hard, see todo in the code).

--- pkg/progress/run.go
+++ pkg/progress/run.go
@@ -15,6 +15,7 @@ import (
 	"github.com/syndtr/gocapability/capability"
 
 	"github.com/osbuild/images/pkg/datasizes"
+	"github.com/osbuild/images/pkg/experimentalflags"
 	"github.com/osbuild/images/pkg/osbuild"
 )
 
@@ -62,6 +63,10 @@ func RunOSBuild(pb ProgressBar, manifest []byte, exports []string, opts *OSBuild
 		return fmt.Errorf("cannot check priviledges: %w", err)
 	}
 	if !enoughPrivs {
+		if experimentalflags.Bool("supermin") {
+			fmt.Fprintf(os.Stderr, "WARNING: using experimental supermin to build\n")
+			return runOSBuildWithSupermin(pb, manifest, exports, opts)
+		}
 		return fmt.Errorf("not enough priviledges: must be root with CAP_SYS_ADMIN")
 	}
 
